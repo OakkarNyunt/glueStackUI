e467dfafe6f708670935e3251945ca1b9f9601c2
Oakkar1998 <oakkarnyunt@gmail.com>
2025-06-24 21:04:27 +0630

    add banner

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,23 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Image } from "expo-image";
-
+// import { StyleSheet } from "react-native";
 import { Box } from "@/components/ui/box";
-// import { Text } from "@/components/ui/text";
-import { Button, ButtonText } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+// import { Button, ButtonText } from "@/components/ui/button";
 // import { Spinner } from '@/components/ui/spinner';
 import { HStack } from "@/components/ui/hstack";
 import { ShoppingCart } from "lucide-react-native";
-import { VStack } from "@/components/ui/vstack";
+// import { VStack } from "@/components/ui/vstack";
 import { Badge, BadgeText } from "@/components/ui/badge";
+
+import Banner from "@/components/custom/banner";
 
 const blurhash =
   "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
 export default function HomeScreen() {
   const notiNum = 19;
-  
+
   return (
     <SafeAreaView>
       <HStack className="justify-between px-5 items-center">
@@ -28,14 +30,24 @@ export default function HomeScreen() {
         />
         <Box className="items-center">
           <Badge
-            className={`z-10 self-end ${notiNum > 9 ? "h-[26px] w-[26px]" : "h-[22px] w-[22px]" }  bg-red-600 rounded-full -mb-3.5 -mr-3.5`}
+            className={`z-10 self-end ${
+              notiNum > 9 ? "h-[26px] w-[26px]" : "h-[22px] w-[22px]"
+            }  bg-red-600 rounded-full -mb-3.5 -mr-3.5`}
             variant="solid"
           >
-            <BadgeText className="text-white">{notiNum > 9 ? "+9": notiNum}</BadgeText>
+            <BadgeText className="text-white">
+              {notiNum > 9 ? "+9" : notiNum}
+            </BadgeText>
           </Badge>
 
           <ShoppingCart color="green" size={24} />
         </Box>
+      </HStack>
+      <Banner />
+
+      <HStack className=" justify-between px-5">
+        <Text>Shop By Category</Text>
+        <Text>See All</Text>
       </HStack>
     </SafeAreaView>
   );
---
+++ components/custom/banner.tsx
@@ -0,0 +1,35 @@
+import { View, Text, StyleSheet } from 'react-native'
+import { HStack } from "@/components/ui/hstack";
+import { Image } from "expo-image";
+import React from 'react'
+
+
+const blurhash =
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
+
+
+const Banner = () => {
+  return (
+    <HStack>
+        <Image
+          style={styles.banner}
+          source={require("@/assets/images/family2.jpg")}
+          placeholder={{ blurhash }}
+          contentFit="cover"
+          transition={1000}
+          
+        />
+      </HStack>
+  )
+}
+
+export default Banner
+
+const styles = StyleSheet.create({
+  banner:{
+    width: "100%",
+    aspectRatio: 16/9,
+    marginVertical: 10,
+  }
+});
+
